545a1bdeaa713760ee87c7f16192649944630aa4
aca56000 <aca56000@gmail.com>
2018-05-17 14:03:56 +0200

    header-nav dans ul

--- src/View/Frontend/template.php
+++ src/View/Frontend/template.php
@@ -35,14 +35,14 @@
                 
         <header class="sticky-top col-12">
             <!-- Navbar ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light header-nav">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
                 <a class="navbar-brand" href="index.php">La Conciergerie du Web</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto ">
+                    <ul class="navbar-nav mr-auto header-nav">
                         <li class="nav-item active">
                             <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
                         </li>
