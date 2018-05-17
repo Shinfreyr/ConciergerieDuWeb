4225f6db7a7861935475f74c09300b5ff9a1ed88
aca56000 <aca56000@gmail.com>
2018-05-17 10:36:24 +0200

    couleur et font dans body
       background-color: #1c1918;
        color:white;
        font-family: "Roboto", sans-serif;

--- src/View/Frontend/template.php
+++ src/View/Frontend/template.php
@@ -25,6 +25,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
         <!-- Stylesheet +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
         <link href="src/Public/css/style.css" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
     </head>
 
