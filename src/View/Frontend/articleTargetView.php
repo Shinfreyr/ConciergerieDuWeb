83813bcc84cd305ad5e3cf03ac51d904a68bcf59
Freyr <fh.lamodiere@gmail.com>
2018-05-17 11:25:51 +0200

    création static de la page offre ciblé

--- src/View/Frontend/articleTargetView.php
+++ src/View/Frontend/articleTargetView.php
@@ -17,11 +17,12 @@
 
 <!-- Jumbotron Seller +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 <div class="jumbotron col-12 col-md-6">
-    <h1 class="display-4">Hello, world!</h1>
-    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <img class="card-img-top" src="src/Public/images/defautVendeur.jpg" alt="Card image cap">
+    <h1 class="display-4">Nom de magasin</h1>
+    <p class="lead">Description Vendeur Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda doloremque ad excepturi quaerat, exercitationem nostrum dolores eius corrupti architecto eligendi minus at. Autem rerum in alias ipsam saepe cum!</p>
     <hr class="my-4">
-    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <h3>Adresse du magazin</h3>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Voir toutes les offres du magazin</a>
 </div>
 
 
