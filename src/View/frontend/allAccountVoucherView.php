aea82fe42dd921c42f45637ba3ba7c174540d61b
aca56000 <aca56000@gmail.com>
2018-06-28 20:50:57 +0200

    icone telechargement et center img

--- src/View/frontend/allAccountVoucherView.php
+++ src/View/frontend/allAccountVoucherView.php
@@ -16,13 +16,13 @@
                         <div class="card voucherCard col-12">
                             <div class="card-body shadowfdbreduc">
                                 <h2 class="card-title">'.htmlspecialchars($db1['nomArticle']).'</h2>
-                                <span class="targetVoucher"><img class="imgAllVoucher" src="src/Public/images/'.htmlspecialchars($db1['imageArticle']).'" alt="Image de l\'article"></span>
+                                <span class="targetVoucher alignimgdroite"><img class="imgAllVoucher" src="src/Public/images/'.htmlspecialchars($db1['imageArticle']).'" alt="Image de l\'article"></span>
                                 <p class="card-text">
                                     Prix avec Réduction: '.htmlspecialchars($db1['reductionPriceArticle']).'&euro;
                                     <hr>
                                     <article class="text-danger">
                                         Date de fin de validité : '.htmlspecialchars($db1['dateVoucher']).
-                                        ' <a href="index.php?action=dlVoucher&idArticle='.htmlspecialchars($db1['idArticle']).'&codeVoucher='.htmlspecialchars($db1['codeVoucher']).'" class="btn btn-TywebButton float-right voucher margebtn">Télécharger <i class="fa fa-search vuebutton"></i></a>
+                                        ' <a href="index.php?action=dlVoucher&idArticle='.htmlspecialchars($db1['idArticle']).'&codeVoucher='.htmlspecialchars($db1['codeVoucher']).'" class="btn btn-TywebButton float-right voucher margebtn">Télécharger <i class="fa fa-download vuebutton"></i></a>
                                           <a href="index.php?action=supressVoucher&idVoucher='.htmlspecialchars($db1['idVoucher']).'" class="btn btn-danger float-right voucher margebtn">Supprimer</a>
                                     </article>
                                 </p>                
