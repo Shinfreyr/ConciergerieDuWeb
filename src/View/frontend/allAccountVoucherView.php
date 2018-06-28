60c6acf76cfe2b9ad77667420d313b054c9d2175
aca56000 <aca56000@gmail.com>
2018-06-28 15:04:19 +0200

    maj correctif orthographe

--- src/View/frontend/allAccountVoucherView.php
+++ src/View/frontend/allAccountVoucherView.php
@@ -3,22 +3,28 @@
 <?php ob_start(); ?>
 
     <!-- Voucher List ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
-    <div class="card content">
-        <div class="card-body scroll">
+    <span>
+            <h1 class="MgHtBs colorfontD">MES BONS DE REDUCTION</h1>
+            <div class="dropdown-divider Pttrait" >
+            </div>
+    </span>
+   
+    <div class="card content justify-content-center">
+        <div class="card-body scroll col">
             <?php    
                 while ($db1 = $request->fetch()) {
                     echo '
-                        <div class="card border-primary voucherCard">
-                            <div class="card-body targetVoucher">
-                                <h4 class="card-title">'.htmlspecialchars($db1['nomArticle']).'</h4>
-                                <img class="imgAllVoucher" src="src/Public/images/'.htmlspecialchars($db1['imageArticle']).'" alt="Image de l\'article">
+                        <div class="card voucherCard col-12">
+                            <div class="card-body shadowfdbreduc">
+                                <h2 class="card-title">'.htmlspecialchars($db1['nomArticle']).'</h2>
+                                <span class="targetVoucher"><img class="imgAllVoucher" src="src/Public/images/'.htmlspecialchars($db1['imageArticle']).'" alt="Image de l\'article"></span>
                                 <p class="card-text">
                                     Prix avec Réduction: '.htmlspecialchars($db1['reductionPriceArticle']).'&euro;
                                     <hr>
-                                    <article>
+                                    <article class="text-danger">
                                         Date de fin de validité : '.htmlspecialchars($db1['dateVoucher']).
-                                        '<a href="index.php?action=supressVoucher&idVoucher='.htmlspecialchars($db1['idVoucher']).'" class="btn btn-danger float-right voucher">Supprimer</a>
-                                        <a href="index.php?action=voucher&idArticle='.htmlspecialchars($db1['idArticle']).'&endDate='.htmlspecialchars($db1['endDateArticle']).'" class="btn btn-success float-right voucher">Voir le Bon</a>
+                                        ' <a href="index.php?action=voucher&idArticle='.htmlspecialchars($db1['idArticle']).'&endDate='.htmlspecialchars($db1['endDateArticle']).'" class="btn btn-TywebButton float-right voucher margebtn">Télécharger <i class="fa fa-search vuebutton"></i></a>
+                                          <a href="index.php?action=supressVoucher&idVoucher='.htmlspecialchars($db1['idVoucher']).'" class="btn btn-danger float-right voucher margebtn">Supprimer</a>
                                     </article>
                                 </p>                
                             </div>
